03c030e76ab0e065e1e79b6f600b1eace4850c22
crsstat <crsstat@users.noreply.github.com>
2015-09-12 01:40:30 +0100

    Reduced number of processes forked
    1. Combined various commands into single awk commands
    2. Changed sed command to awk for consistent functionality, new method also compatible with more shells as not relying on built-in shell regex functionality
    3. Added nawk check to use extended awk functionality (e.g. gsub) on Solaris

--- run_edb360.sh
+++ run_edb360.sh
@@ -1,19 +1,20 @@
 # edb360 collector
 echo "Start edb360."
-for INST in $(ps axo cmd | grep ora_pmo[n] | sed 's/^ora_pmon_//' | grep -v 'sed '); do
-        if [ $INST = "$( cat /etc/oratab | grep -v ^# | grep -v ^$ | awk -F: '{ print $1 }' | grep $INST )" ]; then
+type nawk 1>/dev/null 2>&1 && AWK=nawk || AWK=awk
+for INST in $(ps axo cmd | $AWK '$0~/^ora_pmon_/ {gsub(/^ora_pmon_/,"",$0); print $0}'); do
+        if [ -n "$( $AWK -F: -v inst=$INST '$1==inst {print $1}' /etc/oratab )" ]; then
                 echo "$INST: instance name = db_unique_name (single instance database)"
                 export ORACLE_SID=$INST; export ORAENV_ASK=NO; . oraenv
         else
                 # remove last char (instance nr) and look for name again
-                LAST_REMOVED=$(echo "${INST:0:$(echo ${#INST}-1 | bc)}")
-                if [ $LAST_REMOVED = "$( cat /etc/oratab | grep -v ^# | grep -v ^$ | awk -F: '{ print $1 }' | grep $LAST_REMOVED )" ]; then
+                LAST_REMOVED="$( $AWK -F: -v inst=$INST '$1==substr(inst,0,length(inst)-1) {print $1}' /etc/oratab )"
+                if [ -n "$LAST_REMOVED" ]; then
                         echo "$INST: instance name with last char removed = db_unique_name (RAC: instance number added)"
                         export ORACLE_SID=$LAST_REMOVED; export ORAENV_ASK=NO; . oraenv; export ORACLE_SID=$INST
-                elif [[ "$(echo $INST | sed 's/.*\(_[12]\)/\1/')" =~ "_[12]" ]]; then
+                elif [ -n "$( echo $INST | $AWK '$0~/_[12]$/' )" ]; then
                         # remove last two chars (rac one node addition) and look for name again
-                        LAST_TWO_REMOVED=$(echo "${INST:0:$(echo ${#INST}-2 | bc)}")
-                        if [ $LAST_TWO_REMOVED = "$( cat /etc/oratab | grep -v ^# | grep -v ^$ | awk -F: '{ print $1 }' | grep $LAST_TWO_REMOVED )" ]; then
+                        LAST_TWO_REMOVED="$( $AWK -F: -v inst=$INST '$1==substr(inst,0,length(inst)-2) {print $1}' /etc/oratab )"
+                        if [ -n "$LAST_TWO_REMOVED" ]; then
                                 echo "$INST: instance name with either _1 or _2 removed = db_unique_name (RAC one node)"
                                 export ORACLE_SID=$LAST_TWO_REMOVED; export ORAENV_ASK=NO; . oraenv; export ORACLE_SID=$INST
                         fi
@@ -32,6 +33,6 @@ EOF
 done
 zip -qmT esp_requirements_host.zip res_requirements_*.txt esp_requirements_*.csv cpuinfo_model_name.txt 
 zip -qmT edb360_output.zip edb360_*.zip esp_requirements_host.zip
-zip -r osw_output.zip `ps -ef | grep OSW | grep FM | awk -F 'OSW' '{print $2}' | cut -f 3 -d ' '`
+zip -r osw_output.zip `ps -ef | awk -F 'OSW' '$0~/OSW/ && $0~/FM/ {split($2,x," "); print x[3]}'`
 
 echo "End edb360 collector. Output: edb360_output.zip and osw_output.zip"
